ffa86a900cd5f2e51e970b3601824a5bbf44a83d
Daniel Bezerra <bezerra@atakama.com>
2026-01-20 11:05:03 -0300

    fix build

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { dirname, resolve } from "node:path";
 import { fileURLToPath } from "node:url";
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
+var __dirname = dirname(fileURLToPath(import.meta.url));
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": resolve(__dirname, "src"),
+    plugins: [react()],
+    resolve: {
+        alias: {
+            "@": resolve(__dirname, "src"),
+        },
     },
-  },
 });
-
